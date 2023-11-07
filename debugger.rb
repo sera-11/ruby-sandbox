986ab23313ada8593b00bdca8a5ed05600faffd7
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2023-11-07 22:11:59 +0000

    practice

---
+++ debugger.rb
@@ -0,0 +1,11 @@
+require "pry-byebug"
+
+lucky_num = rand(1..20)
+
+first_part = "Your lucky number for today is "
+
+last_part = "!"
+
+pp first_part + lucky_num.to_s + last_part
+
+byebug
---
+++ howdy.rb
@@ -0,0 +1,8 @@
+require "active_support/all"
+require "./goodbye.rb" 
+
+# my_string = "Howdy"
+# pp my_string
+
+x = 1
+pp x.ordinalize
